e42bd6ffe09d0d0624e866054c82bd4749d6ef9e
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2025-02-03 15:30:30 +0100

    hours of regex later, small progress!

---
+++ d03/d03_part2.ts
@@ -0,0 +1,25 @@
+// import input from "./input.txt";
+let input: string = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)uno()?mul(8,5))"
+
+function processAssignment(assignment: string) {
+    const pattern: RegExp = /mul\(\d{1,3},\d{1,3}\)/g;
+    const results: string[] | null = assignment.match(pattern);
+
+    if (results) {
+        const sumOfProducts: number = results.reduce((sum: number, opString: string) => {
+            const opNumbers: string[] = opString.replace("mul(", "").replace(")", "").split(",");
+            const product: number = Number(opNumbers[0]) * Number(opNumbers[1]);
+            return sum + product;
+        }, 0);
+        return sumOfProducts;
+    }
+}
+
+// pattern: do() ... don't() or don't() ... don't() or don't() ... STRING_END
+const invalidPattern: RegExp = /don't\(\).+?((do|don't)\(\)|$)/g;
+
+const invalidSubstrings: string[] | null = input.match(invalidPattern);
+invalidSubstrings?.forEach(invalidSubStr => input = input.replace(invalidSubStr, ""))
+
+console.log(input);
+console.log(processAssignment(input));
